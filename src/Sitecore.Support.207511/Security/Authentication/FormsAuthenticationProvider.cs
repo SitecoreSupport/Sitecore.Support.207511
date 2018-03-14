e7e15612e31c6b783a38137577191237276b0b00
Paul Stupka <psk@sitecore.net>
2018-03-14 11:45:22 +0200

    Fix field acces using reflection

--- src/Sitecore.Support.207511/Security/Authentication/FormsAuthenticationProvider.cs
+++ src/Sitecore.Support.207511/Security/Authentication/FormsAuthenticationProvider.cs
@@ -1,13 +1,14 @@
 ﻿namespace Sitecore.Support.Security.Authentication
 {
   using System.Reflection;
+  using Sitecore.Security.Authentication;
   public class FormsAuthenticationProvider : Sitecore.Security.Authentication.FormsAuthenticationProvider
   {
     public override void Initialize(string name, System.Collections.Specialized.NameValueCollection config)
     {
       base.Initialize(name, config);
       var helperFieldInfo =
-        typeof(FormsAuthenticationProvider).GetField("helper", BindingFlags.Instance | BindingFlags.NonPublic);
+        typeof(AuthenticationProvider).GetField("helper", BindingFlags.Instance | BindingFlags.NonPublic);
       if (helperFieldInfo == null)
       {
         Sitecore.Diagnostics.Log.Error("Failed to apply support patch 207511", this);
