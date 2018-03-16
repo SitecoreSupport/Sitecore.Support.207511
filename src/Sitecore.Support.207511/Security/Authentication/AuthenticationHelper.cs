530cc776d5bb3695e67fd584ef8415ec829927e6
Paul Stupka <psk@sitecore.net>
2018-03-16 17:49:08 +0200

    It appears we have two helper fields on a single object

---
+++ src/Sitecore.Support.207511/Security/Authentication/AuthenticationHelper.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Web;
+using Sitecore.Diagnostics;
+using Sitecore.Security.Accounts;
+using Sitecore.Security.Authentication;
+
+namespace Sitecore.Support.Security.Authentication
+{
+  public class AuthenticationHelper : Sitecore.Security.Authentication.AuthenticationHelper
+  {
+    public AuthenticationHelper(AuthenticationProvider provider) : base(provider)
+    {
+
+    }
+
+    public override void SetActiveUser(User user)
+    {
+      Assert.ArgumentNotNull(user, "user");
+      HttpContext current = HttpContext.Current;
+      if (current != null)
+      {
+        try
+        {
+          current.User = user;
+        }
+        catch (NullReferenceException nre)
+        {
+          Log.Error("Sitecore.Support: failed to set HttpContext.Current.User. Most likely the code is executed on a separate thread and does not have associated HttpContext.", nre, this);
+        }
+
+      }
+      System.Threading.Thread.CurrentPrincipal = user;
+    }
+  }
+}
--- src/Sitecore.Support.207511/Security/Authentication/FormsAuthenticationProvider.cs
+++ src/Sitecore.Support.207511/Security/Authentication/FormsAuthenticationProvider.cs
@@ -9,12 +9,18 @@
       base.Initialize(name, config);
       var helperFieldInfo =
         typeof(AuthenticationProvider).GetField("helper", BindingFlags.Instance | BindingFlags.NonPublic);
-      if (helperFieldInfo == null)
+      var formsHelperFieldInfo =
+        typeof(Sitecore.Security.Authentication.FormsAuthenticationProvider).GetField("helper", BindingFlags.Instance | BindingFlags.NonPublic);
+
+      if (helperFieldInfo == null || formsHelperFieldInfo == null)
       {
-        Sitecore.Diagnostics.Log.Error("Failed to apply support patch 207511", this);
+        Sitecore.Diagnostics.Log.Error("Sitecore.Support: Failed to apply support patch 207511", this);
         return;
       }
-      helperFieldInfo.SetValue(this, new FormsAuthenticationHelper(this));
+      helperFieldInfo.SetValue(this, new AuthenticationHelper(this));
+      Sitecore.Diagnostics.Log.Warn("Sitecore.Support: AuthenticationHelper has been overridden", this);
+      formsHelperFieldInfo.SetValue(this, new FormsAuthenticationHelper(this));
+      Sitecore.Diagnostics.Log.Warn("Sitecore.Support: FormsAuthenticationHelper has been overridden", this);
     }
   }
 }
